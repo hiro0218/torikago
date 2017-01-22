d3766228cca54193e9ef1efa0abeaf18fce9b6b6
hiro0218 <hiro0218@gmail.com>
2017-01-23 02:50:09 +0900

    add: UserAgentを設定
    iOS10 + Google Chrome

--- app/src/components/TwiMain.vue
+++ app/src/components/TwiMain.vue
@@ -6,7 +6,8 @@
 </style>
 
 <template>
-  <webview id="webview"></webview>
+  <webview id="webview"
+           useragent="Mozilla/5.0 (iPhone; CPU iPhone OS 10_0_1 like Mac OS X) AppleWebKit/601.1 (KHTML, like Gecko) CriOS/53.0.2785.86 Mobile/14A403 Safari/601.1.46"></webview>
 </template>
 
 <script>
@@ -26,7 +27,7 @@
         var webview = document.getElementById('webview');
 
         window.addEventListener('load', function() {
-          webview.setAttribute('src', 'https://mobile.twitter.com/home');
+          webview.setAttribute('src', 'https://mobile.twitter.com');
           self.setExternalLink(webview);
           self.setStyle(webview);
         }, false);
