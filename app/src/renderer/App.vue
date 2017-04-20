2e4e720935f2fc2f16cb45ebf06677be1b870250
hiro0218 <hiro0218@gmail.com>
2017-04-20 22:32:58 +0900

    fix: D&Dでの遷移を無効化

--- app/src/renderer/App.vue
+++ app/src/renderer/App.vue
@@ -48,4 +48,13 @@
   export default {
     store
   }
+
+  document.addEventListener('drop', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+  });
+  document.addEventListener('dragover', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+  });
 </script>
